fe5e002dc9072f188fa2bc294f402eec311490bc
sushant1990ss <88759526+sushant1990ss@users.noreply.github.com>
2023-08-03 11:10:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require("./app");
-const http = require("http").createServer(app);
-// const server = http.createServer(app);
+const http = require("http");
+const server = http.createServer(app);
 
-const io = require("socket.io")(http, {
+const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
 
 
-// app.set('socketio', io);
+app.set('socketio', io);
 
 io.on("connection", (socket) => {
   console.log("Connected!");
